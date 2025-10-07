4245a7fd4ec9014c8c9d0dd256647481b13f7a82
karelle <karelle.balthazar@ynov.com>
2025-10-07 22:20:13 +0200

    image

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/play", handlePlay)
 	http.HandleFunc("/reset", handleReset)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
