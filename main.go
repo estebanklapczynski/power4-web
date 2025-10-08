e68202279f3167388826bc054d42300e4c59f2ce
Dylan-MOUTY <dylanmouty77@gmail.com>
2025-10-08 16:04:55 +0200

    page2fonctionelle

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,8 +16,10 @@ func main() {
 	currentGame = game.NewGame()
 
 	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/game", handleGame)
 	http.HandleFunc("/play", handlePlay)
 	http.HandleFunc("/reset", handleReset)
+	http.HandleFunc("/restart", handleRestart)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Println("Serveur lancé sur http://localhost:8080")
@@ -26,6 +29,18 @@ func main() {
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/Page1.html"))
 	tmpl.Execute(w, currentGame)
+}
+
+func handleGame(w http.ResponseWriter, r *http.Request) {
+	// Affiche currentGame en JSON lisible dans une balise <pre>
+	b, err := json.MarshalIndent(currentGame, "", "  ")
+	if err != nil {
+		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	escaped := template.HTMLEscapeString(string(b))
+	_, _ = w.Write([]byte("<!doctype html><html><head><meta charset=\"utf-8\"><title>Game</title></head><body><h1>État du jeu</h1><pre>" + escaped + "</pre><p><a href=\"/\">Retour</a></p></body></html>"))
 }
 
 func handlePlay(w http.ResponseWriter, r *http.Request) {
