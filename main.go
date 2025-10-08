fc4e1a3da783ca94bad6ef16ffd3bfd842b43306
Dylan-MOUTY <dylanmouty77@gmail.com>
2025-10-08 16:45:56 +0200

    marcheplus

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// ...existing code...
 package main
 
 import (
@@ -9,10 +10,9 @@ import (
 	"power4-web/game"
 )
 
-var g = game.NewGame()
+var currentGame *game.Game
 
 func main() {
-<<<<<<< HEAD
 	currentGame = game.NewGame()
 
 	http.HandleFunc("/", handleIndex)
@@ -20,21 +20,15 @@ func main() {
 	http.HandleFunc("/play", handlePlay)
 	http.HandleFunc("/reset", handleReset)
 	http.HandleFunc("/restart", handleRestart)
-=======
-	http.HandleFunc("/", serveStart)
-	http.HandleFunc("/game", serveGame)
-	http.HandleFunc("/play", playMove)
->>>>>>> 1fe4eba99b3f469bb529b2ad647312a742b204d9
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-<<<<<<< HEAD
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/Page1.html"))
-	tmpl.Execute(w, currentGame)
+	_ = tmpl.Execute(w, currentGame)
 }
 
 func handleGame(w http.ResponseWriter, r *http.Request) {
@@ -43,36 +37,22 @@ func handleGame(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlePlay(w http.ResponseWriter, r *http.Request) {
-=======
-func serveStart(w http.ResponseWriter, _ *http.Request) {
-	t := template.Must(template.ParseFiles("templates/start.html"))
-	_ = t.Execute(w, nil)
-}
-
-func serveGame(w http.ResponseWriter, _ *http.Request) {
-	t := template.Must(template.ParseFiles("templates/index.html"))
-	_ = t.Execute(w, g)
-}
-
-func playMove(w http.ResponseWriter, r *http.Request) {
->>>>>>> 1fe4eba99b3f469bb529b2ad647312a742b204d9
 	if r.Method == http.MethodPost {
 		if col, err := strconv.Atoi(r.FormValue("column")); err == nil {
-			g.PlayMove(col)
+			currentGame.PlayMove(col)
 		}
 	}
-<<<<<<< HEAD
-	http.Redirect(w, r, "/game", http.StatusSeeOther) // redirige maintenant vers la page du jeu
+	http.Redirect(w, r, "/game", http.StatusSeeOther)
 }
 
 func handleReset(w http.ResponseWriter, r *http.Request) {
 	currentGame = game.NewGame()
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
 func handleRestart(w http.ResponseWriter, r *http.Request) {
 	currentGame = game.NewGame()
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-=======
-	http.Redirect(w, r, "/game", http.StatusSeeOther)
->>>>>>> 1fe4eba99b3f469bb529b2ad647312a742b204d9
 }
+
+// ...existing code...
