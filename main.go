e6af6e6387a2614f82a20b2d57ab4c3d7f493611
ekpro07s@gmail.com <esteban.klapczynski@ynov.com>
2025-10-14 15:11:48 +0200

    sauve

--- main.go
+++ main.go
@@ -1,57 +1,56 @@
-// ...existing code...
 package main
 
 import (
-    "html/template"
-    "log"
-    "net/http"
-    "strconv"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 
-    "power4-web/game"
+	"power4-web/game"
 )
 
-var currentGame game.Game
+var currentGame *game.Game
 
 func main() {
-    currentGame = game.NewGame()
+	currentGame = game.NewGame()
 
-    http.HandleFunc("/", handleIndex)
-    http.HandleFunc("/game", handleGame) // nouvelle route
-    http.HandleFunc("/play", handlePlay)
-    http.HandleFunc("/reset", handleReset)
-    http.HandleFunc("/restart", handleRestart)
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/game", handleGame) // nouvelle route
+	http.HandleFunc("/play", handlePlay)
+	http.HandleFunc("/reset", handleReset)
+	http.HandleFunc("/restart", handleRestart)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    log.Println("Serveur lancé sur http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Serveur lancÃ© sur http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handleIndex(w http.ResponseWriter, rhttp.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/Page1.html"))
-    tmpl.Execute(w, currentGame)
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/Page1.html"))
+	tmpl.Execute(w, currentGame)
 }
 
-func handleGame(w http.ResponseWriter, r http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/index.html"))
-    _ = tmpl.Execute(w, currentGame)
+func handleGame(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	_ = tmpl.Execute(w, currentGame)
 }
 
-func handlePlay(w http.ResponseWriter, rhttp.Request) {
-    if r.Method == http.MethodPost {
-        colStr := r.FormValue("column")
-        col, err := strconv.Atoi(colStr)
-        if err == nil {
-            currentGame.PlayMove(col)
-        }
-    }
-    http.Redirect(w, r, "/game", http.StatusSeeOther) // redirige maintenant vers la page du jeu
+func handlePlay(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		colStr := r.FormValue("column")
+		col, err := strconv.Atoi(colStr)
+		if err == nil {
+			currentGame.PlayMove(col)
+		}
+	}
+	http.Redirect(w, r, "/game", http.StatusSeeOther) // redirige maintenant vers la page du jeu
 }
 
-func handleReset(w http.ResponseWriter, r http.Request) {
-    currentGame = game.NewGame()
-    http.Redirect(w, r, "/", http.StatusSeeOther)
+func handleReset(w http.ResponseWriter, r *http.Request) {
+	currentGame = game.NewGame()
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-func handleRestart(w http.ResponseWriter, rhttp.Request) {
-    currentGame = game.NewGame()
-    http.Redirect(w, r, "/", http.StatusSeeOther)
+func handleRestart(w http.ResponseWriter, r *http.Request) {
+	currentGame = game.NewGame()
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
